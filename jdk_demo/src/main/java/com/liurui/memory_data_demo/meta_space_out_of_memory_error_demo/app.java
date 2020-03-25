da14298b6ded5109c251ab6f98e76e53839f9568
liurui <liurui@daling.com>
2020-03-25 18:34:30 +0800

    init

--- jdk_demo/src/main/java/com/liurui/memory_data_demo/meta_space_out_of_memory_error_demo/app.java
+++ jdk_demo/src/main/java/com/liurui/memory_data_demo/meta_space_out_of_memory_error_demo/app.java
@@ -1,4 +1,30 @@
 package com.liurui.memory_data_demo.meta_space_out_of_memory_error_demo;
 
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * -XX:MaxMetaspaceSize=20m
+ */
 public class app {
+
+
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+
+        enhancer.setSuperclass(app.class);
+        enhancer.setUseCache(false);
+        enhancer.setCallback(new MethodInterceptor() {
+            @Override
+            public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+                return proxy.invokeSuper(obj, args);
+            }
+        });
+        while (true) {
+            enhancer.create();
+        }
+    }
 }
