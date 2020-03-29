8343d08b2ecea51204d541cdfec7de43b8574e42
liurui <1313475@qq.com>
2020-03-29 22:00:06 +0800

    init

--- jdk_demo/src/main/java/com/liurui/memory_data_demo/heap_out_of_memory_demo/app.java
+++ jdk_demo/src/main/java/com/liurui/memory_data_demo/heap_out_of_memory_demo/app.java
@@ -2,12 +2,15 @@ package com.liurui.memory_data_demo.heap_out_of_memory_demo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * 通过jvisualvm分析堆溢出
- * 1. 配置vm,-Xms5m -Xmx5m -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDetails
+ * 1. 配置vm,-Xms5m -Xmx5m -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./a/
  */
 public class app {
+    private  int a = new Random().nextInt();
+
     public static void main(String[] args) {
         List<app>  list = new ArrayList<>();
 
